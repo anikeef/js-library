95ce29d61d60767021e2de9c7048e8fd71efbab0
Gennadiy <genaydzhan70@gmail.com>
2019-06-30 21:26:56 +0600

    Allow adding books with form

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-function Book(title, author, pages) {
+const bookInput = document.forms.book.elements;
+let library = [];
+
+///////// Functions /////////
+/////////////////////////////
+
+function Book(title, author) {
   this.title = title;
   this.author = author;
-  this.pages = pages;
-  this.info = function() {
-    let info = `${this.title}, ${this.author}, ${this.pages} pages`
-    return info;
-  }
+}
+
+function addBookToLibrary() {
+  book = new Book(bookInput.title.value, bookInput.author.value);
+  renderBook(book);
+  library.push(book);
+}
+
+function renderBook(book) {
+  titleText = document.createTextNode(book.title);
+  titleElement = document.createElement("h2");
+  titleElement.appendChild(titleText);
+
+  authorText = document.createTextNode(book.author);
+  authorElement = document.createElement("div.author");
+  authorElement.appendChild(authorText);
+
+  box = document.createElement("div");
+  box.classList.add("book");
+  box.appendChild(titleElement);
+  box.appendChild(authorElement);
+
+  booksContainer = document.querySelector(".books");
+  booksContainer.appendChild(box);
 }
