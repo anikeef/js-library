7084a161967207d7aa60163edc483389d6c68aa9
Gennadiy <genaydzhan70@gmail.com>
2019-07-02 16:26:50 +0600

    Add delete button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const bookInput = document.forms.book.elements;
-let library = [];
+let library = [new Book("book1", "author1"), new Book("book2", "author2")];
+
+renderBooks(library);
 
 ///////// Functions /////////
 /////////////////////////////
@@ -11,24 +13,45 @@ function Book(title, author) {
 
 function addBookToLibrary() {
   book = new Book(bookInput.title.value, bookInput.author.value);
-  renderBook(book);
+  renderBook(book, library.length);
   library.push(book);
 }
 
-function renderBook(book) {
+function deleteBook(event) {
+  id = event.target.value;
+  bookElement = document.getElementById(id);
+  bookElement.remove();
+  delete library[id];
+}
+
+function renderBook(book, index) {
   titleText = document.createTextNode(book.title);
   titleElement = document.createElement("h2");
   titleElement.appendChild(titleText);
 
   authorText = document.createTextNode(book.author);
-  authorElement = document.createElement("div.author");
+  authorElement = document.createElement("div");
   authorElement.appendChild(authorText);
+
+  deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete-btn");
+  deleteButton.value = index;
+  deleteButton.innerHTML = "delete";
+  deleteButton.addEventListener("click", deleteBook);
 
   box = document.createElement("div");
   box.classList.add("book");
+  box.id = index;
   box.appendChild(titleElement);
   box.appendChild(authorElement);
+  box.appendChild(deleteButton);
 
   booksContainer = document.querySelector(".books");
   booksContainer.appendChild(box);
+}
+
+function renderBooks(library) {
+  for (let i = 0; i < library.length; ++i) {
+    if (library[i]) renderBook(library[i], i);
+  }
 }
